1d407bcd6546b551ce6f0466713eca07d85cf835
Woozle44 <will.m.taylor44@gmail.com>
2013-08-12 22:41:19 +0100

    Added D20 crits

--- js/rollD20.js
+++ js/rollD20.js
@@ -3,5 +3,11 @@ function rollD20(){
 					var status = document.getElementById("status");
 					var d20 = Math.floor(Math.random() * 20) + 1;
 					die6.innerHTML = d20;
-					status.innerHTML = "You rolled "+d20+"."; 
+					status.innerHTML = "You rolled "+d20+".";
+					if (d20 == 1) {
+						status.innerHTML = "You rolled "+d20+", CRITICAL MISS LOLOLOLOL";
+					}
+					else if (d20 == 20){
+						status.innerHTML = "You rolled "+d20+", CRITICAL HIT W00T";
+						}
 				}
