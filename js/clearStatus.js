935640391bedf95025eb3687612aa41417f3c668
Woozle44 <will.m.taylor44@gmail.com>
2013-08-12 15:29:52 +0100

    Added Clear Button

---
+++ js/clearStatus.js
@@ -0,0 +1,16 @@
+function clearStatus() {
+						var status = document.getElementById("status");
+						var die1 = document.getElementById("die1");
+						var die2 = document.getElementById("die2");
+						var die3 = document.getElementById("die3");
+						var die4 = document.getElementById("die4");
+						var die5 = document.getElementById("die5");
+						var die6 = document.getElementById("die6");
+						status.innerHTML = "";
+						die1.innerHTML = "0";
+						die2.innerHTML = "0";
+						die3.innerHTML = "0";
+						die4.innerHTML = "0";
+						die5.innerHTML = "0";
+						die6.innerHTML = "0";
+						}
